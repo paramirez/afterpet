18218cdb9110914dbc4a52e9f0f1be67f626172e
Pablo Ramirez <pablo.ramirez@smartsoft.com.co>
2018-02-24 18:11:35 -0500

    dwq

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDzIiL2Ksb1I69RD5JtzUyuvF_rZzb_x5Q",
+    authDomain: "after-pet.firebaseapp.com",
+    databaseURL: "https://after-pet.firebaseio.com",
+    projectId: "after-pet",
+    storageBucket: "after-pet.appspot.com",
+    messagingSenderId: "904119751496"
+};
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+import firebase from "firebase";
+import { firebaseConfig } from "./config";
+
 import Vue from "vue";
 import App from "./App.vue";
-import { router } from "./router";
-import { store } from "./store";
+
+import { router } from "./router"; //Rutas
+import { store } from "./store"; //Store
 
 Vue.config.productionTip = false;
+
+firebase.initializeApp(firebaseConfig); //Inicializa app firebase
 
 new Vue({
     router,
